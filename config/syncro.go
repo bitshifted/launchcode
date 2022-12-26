0669c12e00b4576e2986b487d54877eea21fe01e
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2022-12-26 14:52:11 +0100

    improve launcher update

--- config/syncro.go
+++ config/syncro.go
@@ -1,12 +1,14 @@
 package config
 
+import "fmt"
+
 const (
 	syncroMainClass = "co.bitshifted.appforge.syncro.Syncro"
 )
 
-func GetSyncroCmdOptions() []string {
+func GetSyncroCmdOptions(launcherFilePath string) []string {
 	options := make([]string, 0)
 	options = append(options, setSplashScreen()...)
-	options = append(options, []string{"-classpath", "cp/*", syncroMainClass}...)
+	options = append(options, []string{"-classpath", "cp/*", syncroMainClass, fmt.Sprintf("--launcher-file=%s", launcherFilePath)}...)
 	return options
 }
--- processor.go
+++ processor.go
@@ -6,22 +6,27 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 const (
 	retryFilesName      = "retries.txt"
 	deleteRetryFileName = "deletes.txt"
 	entryDelimiter      = "->"
+	cleanupDelaySeconds = 5
+	oldExtension        = ".old"
 )
+
+var launcherUpdated = false
 
 type ProcessEntry struct {
 	sourcePath string
 	targetPath string
 }
 
-func processRetryFiles() {
+func processRetryFiles(launcherFilePath string) {
 	processDeletes()
-	processUpdates()
+	processUpdates(launcherFilePath)
 }
 
 func processDeletes() {
@@ -52,7 +57,7 @@ func processDeletes() {
 	}
 }
 
-func processUpdates() {
+func processUpdates(launcherFilePath string) {
 	//  read file if present
 	readFile, err := os.Open(retryFilesName)
 	defer readFile.Close()
@@ -71,6 +76,9 @@ func processUpdates() {
 	}
 	// perform copy
 	for _, entry := range entries {
+		if entry.targetPath == launcherFilePath {
+			processLauncherUpdate(entry)
+		}
 		log.Printf("Copying file %s to %s\n", entry.sourcePath, entry.targetPath)
 		source, err := os.Open(entry.sourcePath)
 		if err != nil {
@@ -78,7 +86,7 @@ func processUpdates() {
 			continue
 		}
 		defer source.Close()
-		target, err := os.OpenFile(entry.targetPath, os.O_RDWR, 0666)
+		target, err := os.OpenFile(entry.targetPath, os.O_RDWR|os.O_CREATE, 0666)
 		if err != nil {
 			log.Printf("Failed to open target file %s. Error: %s\n", entry.targetPath, err)
 			continue
@@ -86,7 +94,7 @@ func processUpdates() {
 		defer target.Close()
 		_, err = io.Copy(target, source)
 		if err != nil {
-			log.Printf("Failed to writer target file %s. Error: %s\n", entry.targetPath, err)
+			log.Printf("Failed to write target file %s. Error: %s\n", entry.targetPath, err)
 			continue
 		}
 	}
@@ -97,5 +105,29 @@ func lineToEntry(line string) ProcessEntry {
 	return ProcessEntry{
 		sourcePath: parts[0],
 		targetPath: parts[1],
+	}
+}
+
+func processLauncherUpdate(entry ProcessEntry) {
+	log.Println("Processing launcher update")
+	oldPath := entry.targetPath + oldExtension
+	os.Rename(entry.targetPath, oldPath)
+	log.Printf("Current launcher renamed to %s\n", oldPath)
+	launcherUpdated = true
+}
+
+func cleanup(oldLaucnherFile string) {
+	time.Sleep(cleanupDelaySeconds * time.Second)
+	err := os.Remove(retryFilesName)
+	if err != nil {
+		log.Printf("Failed to delete file %s\n", retryFilesName)
+	}
+	err = os.Remove(deleteRetryFileName)
+	if err != nil {
+		log.Printf("Failed to delete file %s\n", deleteRetryFileName)
+	}
+	err = os.Remove(oldLaucnherFile)
+	if err != nil {
+		log.Printf("Failed to delete file %s\n", oldLaucnherFile)
 	}
 }
